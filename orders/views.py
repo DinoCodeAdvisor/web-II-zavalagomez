7ee53cb72e37cad10ab4f34ffb7d56cdd26ef4f8
PlayerAdjustments <ferzg2004@gmail.com>
2025-02-07 20:40:37 -0600

    Order views

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Question,Choice
 
 def indexOrders(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    questions = Question.objects.all()
+
+    data = {
+        "titulo": "Pagina de orders",
+        "total_orders": 100,
+        "total_payments": 200,
+        "orders": [
+            {
+                "id": 1, "total":100
+            },
+            {
+                "id": 2, "total":100
+            },
+            {
+                "id": 3, "total":100
+            },
+            {
+                "id": 4, "total":100
+            },
+        ],
+        "questions": questions
+    }
+    return render(request, 'orders/index.html', data)
