5710f1b31bfcc5300a8d5095918397bb71dc2ad3
PlayerAdjustments <ferzg2004@gmail.com>
2025-02-14 20:16:45 -0600

    Components UserCard && Templates

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.usersIndex, name="usersIndex"),
     path("cool", views.usersCool, name="usersCool"),
+    path("create", views.createUserView, name="createUserView"),
+    path("createUser", views.createUser, name="createUser"),
+    path("details/<int:id>", views.userDetail, name="userDetail")
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Users, UsersAddress
@@ -20,3 +20,33 @@ def usersCool(request):
     }
 
     return render(request, 'users/cool.html', data)
+
+def createUserView(request):
+    return render(request, "users/create.html")
+
+def createUser(request):
+    data = {}
+    try:
+        if request.method == "POST":
+            name = request.POST.get("name")
+            email = request.POST.get("email")
+            age = request.POST.get("age")
+            rfc = request.POST.get("rfc")
+            photo = request.POST.get("photo")
+
+            user = Users(name=name, email=email, age=age, rfc=rfc, photo=photo)
+            user.save()
+
+            data["user"] = user
+            data["message"] = "User created"
+            data["status"] = "success"
+    except Exception as e:
+        data["message"] = str(e)
+        data["status"] = "error"
+    
+    return render(request, "users/create.html", data)
+
+def userDetail(request, id):
+    user = get_object_or_404(Users, id=id)
+    
+    return render(request, "users/details.html", {"user": user})
